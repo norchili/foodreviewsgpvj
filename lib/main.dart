294dd33e098adb68d322a4ccc04df677aea41e83
Norberto Aguilar Ramirez <norchilipeper@hotmail.com>
2021-10-04 12:58:48 -0600

    Se agrego Proyecto a Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,60 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return SomethingWentWrong(error: snapshot.error);
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return const CircularProgressIndicator();
+      },
+    );
+  }
+}
+
+class SomethingWentWrong extends StatelessWidget {
+  final Object? error;
+  const SomethingWentWrong({Key? key, required this.error}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String _error =
+        error == null ? error.toString() : "No hay datos en el error";
+    return Scaffold(
+        body: Center(
+            child: Text(
+                "Error al conectar con Firebase, revisa tu conexion a Internet \nError: $_error")));
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -13,17 +66,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          // Color por default es blue
+          // Font por default es Comfortaa
+          primarySwatch: Colors.blue,
+          fontFamily: 'Comfortaa'),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
